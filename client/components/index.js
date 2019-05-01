79e3f565719307b261c25ea69840f425ddaaec7f
Michaela Burns <michaelaburns0@gmail.com>
2019-05-01 16:59:21 -0400

    changed file names

--- client/components/index.js
+++ client/components/index.js
@@ -4,5 +4,5 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './Navbar'
-export {default as UserHome} from './User-Home'
-export {Login, Signup} from './Auth-Form'
+export {default as UserHome} from './UserHome'
+export {Login, Signup} from './AuthForm'
