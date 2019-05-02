e8075c189e2836d0994be52f5a2733556cdb49f4
Allison French <alliefrench22@gmail.com>
2019-05-02 15:17:47 -0400

    set up codesandbox component. starting redux store file for problems

---
+++ client/sandboxFiles/sandboxIndexHtml.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+export const sandboxHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width initial-scale=1.0" />
+    <title>Algo Rhythms</title>
+    <link rel="stylesheet" href="/index.css" />
+
+  </head>
+  <body style="background-color: grey">
+    <div id="main" style="font-size: large; color: #4286f4; width: 400px; font-family: Arial">
+    <p>
+      Now that you have taken time to consider the problem, generate examples and map out
+      your approach, let's code! </p>
+      <p>Give your idea a thorough workover before checking it against
+      the "tests" tab (found above). If/when you have solved the problem, consider
+      the following:</p>
+      <ul>
+      <li>
+        What is the time and space complexity?
+      </li>
+      <li>
+        Is there opportunity for optimization?
+      </li>
+      <li>
+        Are there any edits you would make for readability?
+      </li>
+      </ul>
+    </div>
+  </body>
+</html>;`
