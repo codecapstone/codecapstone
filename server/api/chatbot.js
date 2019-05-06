b68d20885a44b506f60bd8efd57561c2c4d3eed4
Jenny Grange <jenniferhelengrange@gmail.com>
2019-05-06 16:03:08 -0400

    trying the google docs versions

--- server/api/chatbot.js
+++ server/api/chatbot.js
@@ -1,17 +1,49 @@
-const router = require('express').Router()
+// const router = require('express').Router()
+const {WebhookClient} = require('dialogflow-fulfillment')
+const functions = require('firebase-functions')
 
-module.exports = router
+//Create an instance
 
-router.post('/', async (req, res, next) => {
-  try {
-    let agent = req.body
-    agent.response = 'BLAH BLAH BLAH'
+// module.exports = router
 
-    res.send(agent)
-  } catch (err) {
-    next(err)
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
+  (request, response) => {
+    const agent = new WebhookClient({request, response})
+
+    function example(agent) {
+      const example1 = agent.parameters['example']
+
+      if (example1) {
+        agent.add(`Nice example!`)
+      }
+      agent.add('Tell me your example')
+    }
+
+    let intentMap = new Map()
+    intentMap.set('example', example)
+    agent.handleRequest(intentMap)
   }
-})
+)
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const agent = req.body
+//     console.log('chatbot api', agent)
+//     agent.add = {welcome: 'Blaaah'}
+//     // exports.dialogflowFirebaseFullfilment = functions.https.onRequest(
+//     //   (req, res) => {
+//     //     const agent = new WebhookClient({request: request, response: response})
+//     //     const addResp = agent => {
+//     //       agent.add('BLAH BLAH BLAH')
+//     //     }
+//     //     agent.handleRequest(addResp)
+//     //   }
+//     // )
+
+//     res.send(agent)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 router.get('/', async (req, res, next) => {
   try {
