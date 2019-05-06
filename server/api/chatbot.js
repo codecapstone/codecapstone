2bd5a0c5d84a0db830c18368a05bc50f8d3b791d
Jenny Grange <jenniferhelengrange@gmail.com>
2019-05-06 12:54:03 -0400

    updates chatbot api to send response

--- server/api/chatbot.js
+++ server/api/chatbot.js
@@ -5,6 +5,15 @@ module.exports = router
 router.post('/', async (req, res, next) => {
   try {
     console.log('from the chatbot api', req.body)
+    res.sendStatus(200)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.json('hello')
   } catch (err) {
     next(err)
   }
