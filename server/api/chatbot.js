09290a3ccce667fbb2c100205734fd01a8e90506
Jenny Grange <jenniferhelengrange@gmail.com>
2019-05-06 16:28:42 -0400

    trying again....

--- server/api/chatbot.js
+++ server/api/chatbot.js
@@ -1,54 +1,35 @@
-// const router = require('express').Router()
+const router = require('express').Router()
 const {WebhookClient} = require('dialogflow-fulfillment')
 const functions = require('firebase-functions')
 
 //Create an instance
 
-// module.exports = router
+module.exports = router
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
-  (request, response) => {
-    const agent = new WebhookClient({request, response})
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('req.body in chatbot api', req.body)
+    const agent = new WebhookClient({request: req.body, response: response})
+    console.log('chatbot api', agent)
+    agent.add(`Nice`)
 
-    function example(agent) {
-      const example1 = agent.parameters['example']
+    //     const addResp = agent => {
+    //       agent.add('BLAH BLAH BLAH')
+    //     }
+    //     agent.handleRequest(addResp)
+    //   }
+    // )
 
-      if (example1) {
-        agent.add(`Nice example!`)
-      }
-      agent.add('Tell me your example')
-    }
-
-    let intentMap = new Map()
-    intentMap.set('example', example)
-    agent.handleRequest(intentMap)
+    res.send(agent)
+  } catch (err) {
+    next(err)
   }
-)
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const agent = req.body
-//     console.log('chatbot api', agent)
-//     agent.add = {welcome: 'Blaaah'}
-//     // exports.dialogflowFirebaseFullfilment = functions.https.onRequest(
-//     //   (req, res) => {
-//     //     const agent = new WebhookClient({request: request, response: response})
-//     //     const addResp = agent => {
-//     //       agent.add('BLAH BLAH BLAH')
-//     //     }
-//     //     agent.handleRequest(addResp)
-//     //   }
-//     // )
+})
 
-//     res.send(agent)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// router.get('/', async (req, res, next) => {
-//   try {
-//     res.json('hello')
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+router.get('/', async (req, res, next) => {
+  try {
+    res.json('hello')
+  } catch (err) {
+    next(err)
+  }
+})
