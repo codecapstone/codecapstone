3771ec82e4e249f87ff217481164f5606d746caa
Allison French <alliefrench22@gmail.com>
2019-05-08 14:08:57 -0400

    css tweaks

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -13,15 +13,9 @@ const AuthForm = props => {
     <div>
       <form onSubmit={handleSubmit} name={name} id="login">
         <div>
-          {/* <label htmlFor="email">
-            <small>Email</small>
-          </label> */}
           <input name="email" type="text" placeholder="Email" />
         </div>
         <div>
-          {/* <label htmlFor="password">
-            <small>Password</small>
-          </label> */}
           <input name="password" type="password" placeholder="password" />
         </div>
         <div>
--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import {InterviewBot} from './interviewBot'
 import {Login, Signup} from './AuthForm'
 
 const Navigation = ({handleClick, isLoggedIn, email}) => (
@@ -15,17 +14,13 @@ const Navigation = ({handleClick, isLoggedIn, email}) => (
       {isLoggedIn ? (
         <div className="navLinks">
           {/* The navbar will show these links after you log in */}
-          {/* <div>
-            Welcome, <br />
-            {email}!
-          </div> */}
+          <div>Welcome, {email}!</div>
           <Link to="/home">Home</Link>
           <Link to="/challenges">Challenges</Link>
           <Link to="/lessons">Lessons</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-          {/* <InterviewBot /> */}
         </div>
       ) : (
         <div className="navLinks">
