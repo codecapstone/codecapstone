988a966c0e758a86dd4e1be09d3963bb4a16b9b3
Jenny <jenniferhelengrange@gmail.com>
2019-05-09 18:56:59 -0400

    another merge

--- client/routes.js
+++ client/routes.js
@@ -44,28 +44,18 @@ class Routes extends Component {
         <Route path="/stats" component={UserStats} />
         <Route path="/code" component={Code} />
         <Route path="/solutions" component={Solutions} />
-<<<<<<< HEAD
-        {isLoggedIn && <Switch>
-=======
-        <Route path="/example-check" component={ExampleCheck} />
         {isLoggedIn && (
           <Switch>
->>>>>>> 8601d5591b7a346e06f79a7f281688985f2fed6a
             {/* Routes placed here are only available after logging in */}
             <Route exact path="/" component={UserHome} />
             <Route path="/home" component={UserHome} />
             <Route path="/challenges" component={Challenges} />
             <Route path="/admin" component={PostProblem} />
-<<<<<<< HEAD
             <Route exact path="/topics" component={Topics} />
             <Route exact path="/topics/:topicId" component={SingleTopic} />
             <Route path="/lessons" component={Lessons} />
-          </Switch>}
-=======
-            <Route path="/topics" component={Topics} />
           </Switch>
         )}
->>>>>>> 8601d5591b7a346e06f79a7f281688985f2fed6a
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
       </Switch>
