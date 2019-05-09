f78c35dc1c415dd99de88e3951c9ae85afed35a8
Michaela Burns <michaelaburns0@gmail.com>
2019-05-09 17:42:10 -0400

    css tweaks

--- client/components/Solution.js
+++ client/components/Solution.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Solution = props => {
   return (
-    <div id="individualSolution">
+    <div id="individualSolution" className="userHomeCard">
       Solution #{props.idx + 1}
       <pre>{props.solution}</pre>
     </div>
