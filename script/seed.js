d37b433cde64df14a7c08b2f4dd4c4d29d646d00
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-05-09 21:59:25 -0400

    lesson detail added

--- script/seed.js
+++ script/seed.js
@@ -29,7 +29,8 @@ async function seed() {
       name: 'Fibonacci Number',
       description:
         'In mathematics, the Fibonacci numbers are the numbers in the following integer sequence, called the Fibonacci sequence, and characterized by the fact that every number after the first two is the sum of the two preceding ones: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...',
-      reference: ['https://en.wikipedia.org/wiki/Fibonacci_number']
+      reference: ['https://en.wikipedia.org/wiki/Fibonacci_number'],
+      topicId:1
     })
   ])
   const challenges = await Promise.all([
