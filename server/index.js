96062bbc5f2f46c3efdfd496dbdd219ecc5d6540
Jenny <jenniferhelengrange@gmail.com>
2019-05-12 19:24:20 -0400

    no redirect on 8080

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -43,6 +44,9 @@ passport.deserializeUser(async (id, done) => {
 const createApp = () => {
   // logging middleware
   app.use(morgan('dev'))
+
+  // Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
+  app.use(redirectToHTTPS([/localhost:8080/], [/\/insecure/], 301))
 
   // body parsing middleware
   app.use(express.json())
