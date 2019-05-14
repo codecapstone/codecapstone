e03640dce0e9bcb33c9a4d5fb90d4bc9179e6980
Allison French <alliefrench22@gmail.com>
2019-05-14 10:35:13 -0400

    draft slides

--- client/components/GuestHome.js
+++ client/components/GuestHome.js
@@ -5,6 +5,16 @@ import {Login, Signup, AboutUs} from './index'
 export const GuestHome = props => {
   return (
     <div className="container">
+      <iframe
+        id="slides"
+        src="https://docs.google.com/presentation/d/e/2PACX-1vQI_iHXVuo4SpwUE5GafZX2YuAQMA-oQdK8VWkq4D2R_vITz-i9PfXPhswi59QgkSAVbMsA2Kk0Fhor/embed?start=true&loop=true&delayms=2000&rm=minimal"
+        frameborder="0"
+        width="532"
+        height="300"
+        allowfullscreen="false"
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+      />
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
