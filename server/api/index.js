38109083c8f8db0353bf776a1911d590c79228e6
Jenny Grange <jenniferhelengrange@gmail.com>
2019-05-15 15:46:34 -0400

    fix index cnflict

--- server/api/index.js
+++ server/api/index.js
@@ -7,11 +7,8 @@ router.use('/chatbot', require('./chatbot'))
 router.use('/lessons', require('./lesson'))
 router.use('/topics', require('./topic'))
 router.use('/userstats', require('./userStats'))
-<<<<<<< HEAD
 router.use('/currentChallenge', require('./currentChallenge'))
-=======
 router.use('/userInput', require('./userInput'))
->>>>>>> master
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
