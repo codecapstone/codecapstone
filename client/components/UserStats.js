96d2b177038e8e0ac58069d1d3a4619730b29417
Allison French <alliefrench22@gmail.com>
2019-05-15 10:42:04 -0400

    updated fonts

--- client/components/UserStats.js
+++ client/components/UserStats.js
@@ -15,7 +15,7 @@ export class UserStats extends React.Component {
 
   componentDidMount() {
     const {userId, challengeId} = this.props
-    this.handleInputChange(event, userId, challengeId, this.state.isCompleted)
+    // this.handleInputChange(event, userId, challengeId, this.state.isCompleted)
   }
 
   handleInputChange(event, userId, challengeId, isCompleted) {
@@ -40,7 +40,7 @@ export class UserStats extends React.Component {
     return (
       <div className="borderCard" id="stats">
         <div className="userHomeCard">
-          <p>Your problem was: {problem}</p>
+          <h3>Your problem was: {problem}</h3>
           <p>In your approach you got the following keywords:</p>
           {keywordsGot.map((word, idx) => (
             <p key={idx} id="keywordsGot">
